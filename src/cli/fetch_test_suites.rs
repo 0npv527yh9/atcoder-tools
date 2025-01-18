715cb8afda6d437ce97854e67016895badba161d
Junya Hirashima <jhirashima99@gmail.com>
2025-01-18 15:51:53 +0900

    feat: Implement CLI Command to Fetch Test Cases

---
+++ src/cli/fetch_test_suites.rs
@@ -0,0 +1,25 @@
+use crate::{
+    config::Config,
+    dao::Dao,
+    dto::SessionData,
+    error::UnwrapOrExit,
+    handler::{file_handler, http_handler::HttpHandler},
+    service::fetch_test_case::FetchTestSuitesService,
+};
+
+pub fn fetch(url: &str, config: &Config) {
+    let SessionData {
+        cookies,
+        csrf_token,
+    } = file_handler::load_session_data(&config.file.session_data).unwrap_or_exit();
+
+    let http_handler = HttpHandler::with_cookies(cookies);
+    let dao = Dao::new(http_handler, csrf_token);
+    let fetch_service = FetchTestSuitesService::new(dao);
+
+    let tasks = fetch_service
+        .fetch_test_suites(url, config)
+        .unwrap_or_exit();
+
+    println!("Saved: {tasks:?}");
+}
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,3 +1,4 @@
+mod fetch_test_suites;
 mod login;
 
 use crate::config::Config;
@@ -12,10 +13,18 @@ struct Cli {
 #[derive(Subcommand)]
 enum Command {
     Login,
+
+    /// Fetch test suites
+    #[command(name = "fetch-test", visible_alias = "f")]
+    FetchTestSuites {
+        /// URL of a contest page or a task page
+        url: String,
+    },
 }
 
 pub fn run(config: Config) {
     match Cli::parse().command {
         Command::Login => login::login(&config.url.login, &config.file.session_data),
+        Command::FetchTestSuites { url } => fetch_test_suites::fetch(&url, &config),
     }
 }
